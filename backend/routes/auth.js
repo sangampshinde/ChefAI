c167912f16f5c9db35b308b3e613d04d6ee62580
Sangam Shinde <sangampshinde@gmail.com>
2026-04-15 22:24:41 +0530

    auth rout setup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,3 +3,16 @@ const router = express.Router();
 import * as authController from '../controllers/authController.js'
 import authMiddleware from '../middlewares/auth.js';
 
+// Auth Routes (Public)
+router.post('/signup', authController.register);
+router.post('/login', authController.login);
+router.post('/reset-password', authController.requestPasswordReset);
+
+
+// Protected route import authMiddleware
+router.get('/me', authMiddleware, authController.getCurrentUser);
+
+export default router;
+
+
+
